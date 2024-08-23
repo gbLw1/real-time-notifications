9bde2b8a4e761dd32b63c52dfd8e048ab87abdaa
Gabriel Grassi <gabrielhenrique2504@gmail.com>
2024-08-23 01:52:46 -0300

    feat: createdAt / updatedAt on save

--- Server/src/API/Data/MyDbContext.cs
+++ Server/src/API/Data/MyDbContext.cs
@@ -9,8 +9,28 @@ public class MyDbContext : DbContext
 
     public DbSet<NotificationEntity> Notifications { get; set; }
 
-    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    protected override void OnModelCreating(ModelBuilder builder)
     {
-        modelBuilder.ApplyConfigurationsFromAssembly(assembly: GetType().Assembly);
+        builder.Ignore<BaseEntity>();
+        builder.ApplyConfigurationsFromAssembly(assembly: GetType().Assembly);
+    }
+
+    public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+    {
+        var entries = ChangeTracker.Entries()
+            .Where(e => e.Entity is BaseEntity
+                     && (e.State == EntityState.Added || e.State == EntityState.Modified));
+
+        foreach (var entry in entries)
+        {
+            if (entry.State == EntityState.Added)
+            {
+                ((BaseEntity)entry.Entity).CreatedAt = DateTime.UtcNow;
+            }
+
+            ((BaseEntity)entry.Entity).UpdatedAt = DateTime.UtcNow;
+        }
+
+        return base.SaveChangesAsync(cancellationToken);
     }
 }
