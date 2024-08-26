83ff99d8e33b8e9affa6cd8a0c4ebf0ed910b590
Gabriel Grassi <gabrielhenrique2504@gmail.com>
2024-08-26 15:43:22 -0300

    fix: short ctor

--- Server/src/API/Data/MyDbContext.cs
+++ Server/src/API/Data/MyDbContext.cs
@@ -3,12 +3,8 @@ using RTN.API.Data.Entities;
 
 namespace RTN.API.Data;
 
-public class MyDbContext : DbContext
+public class MyDbContext(DbContextOptions<MyDbContext> options) : DbContext(options)
 {
-    public MyDbContext(DbContextOptions<MyDbContext> options) : base(options) { }
-
-    public DbSet<NotificationEntity> Notifications { get; set; }
-
     protected override void OnModelCreating(ModelBuilder builder)
     {
         builder.Ignore<BaseEntity>();
